c694e459396e84801a76baa284b4c8be94639544
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2025-01-23 01:17:27 -0500

    renderizando datos textuales de consulta al back

--- lib/presentation/view/views/categorias/CategoriasView.dart
+++ lib/presentation/view/views/categorias/CategoriasView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movil/config/routes/app_rutas.dart';
+import 'package:movil/domain/entities/categoriaEntidad.dart';
 import 'package:movil/presentation/view/widgets/bar.dart';
 import 'package:movil/presentation/view/widgets/tarjeta.dart';
 import 'package:movil/presentation/viewmodels/categoriaViewModel.dart';
@@ -15,18 +16,27 @@ class CategoriasView extends StatefulWidget {
 }
 
 class _CategoriasViewState extends State<CategoriasView> {
+  //Definimos el estado al iniciar la vista
   @override
   void initState() {
-    super.initState(); // Llamar a fetchCategories al iniciar la vista
+    super.initState();
+    //nos aseguramos que el contexto este cargado antes de hace el llamdo de los datos
+    //la funcion se ejecuta despues de que se renderice el primer fotograma de la pantalla, esto asegura que el contexto estara cargado
+    //evita errores de contexto
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      //accedemos al provider
       final categoriaViewModel =
+          //la vardera listen:false asegura que no se cargue nuevamente los datos al acceder a la pantalla
+          //para este caso necesitamos que la peticion solo se haga cuando se abre la vista
           Provider.of<CategoriaViewModel>(context, listen: false);
+      //cargamos las categorias
       categoriaViewModel.fetchCategorias();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    //accedemos al provider para usar los datos
     final categoriaViewModel = Provider.of<CategoriaViewModel>(context);
     return Scaffold(
         appBar: Bar(title: 'Categorias'),
@@ -36,15 +46,16 @@ class _CategoriasViewState extends State<CategoriasView> {
   Widget contenedorSeguro(CategoriaViewModel viewModel) {
     return SafeArea(
         child: ListView.builder(
-            itemCount: viewModel.categorias!.length,
+            //la lista se construira segun el tamaño de la lista categorias del viewmodel
+            itemCount: viewModel.categorias?.length ?? 0,
             itemBuilder: (context, index) {
+              final CategoriaEntidad categoria = viewModel.categorias![index];
               return Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Tarjeta(
-                      atrTitulo: 'categoria $index',
-                      atrDescripcion:
-                          'Aute nostrud deserunt ullamco voluptate aliqua anim anim deserunt ullamco nostrud. Labore pariatur ea amet non eu sunt. Esse labore nulla et sint consectetur sunt. Aliqua eu Lorem laboris mollit fugiat cupidatat nisi laboris et non. Quis nisi dolor esse laboris minim tempor veniam nostrud. Ex magna laboris mollit in amet officia quis tempor non.',
+                      atrTitulo: categoria.titulo,
+                      atrDescripcion: categoria.descripcion,
                       atrRutaTarjeta: AppRutas.cursos,
                       atrRutaEdicion: AppRutas.curso),
                 ],
